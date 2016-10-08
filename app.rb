63f89682e370f7ab7fee2a7473eca588afe77641
Tony Amoyal <tonyamoyal@gmail.com>
2016-10-08 12:07:58 -0400

    quick web interface for facebook sharing

---
+++ app.rb
@@ -0,0 +1,54 @@
+require 'json'
+require 'typhoeus'
+require 'uri'
+
+class TinyShowAPI
+	def show(id)
+		data.detect{ |event| event["event"]["id"] == id.to_i }
+	end
+
+	private
+
+	def data
+		@@data ||= JSON.parse(Typhoeus.get("https://s3.amazonaws.com/tinyshow-data/data.json").body)
+	end
+end
+
+class EnvironmentManager
+	def self.get_domain_for_dev_env(env)
+		if env == "development"
+			get_open_ngrok_domain || raise("Start ngrok to use this server in development mode.")
+		else
+			"www.tinyshow.com"
+		end
+	end
+
+	private
+
+	def self.get_open_ngrok_domain
+		body = Typhoeus.get("http://127.0.0.1:4040/api/tunnels").body
+		return nil if body.empty?
+		ngrok_config = JSON.parse(body)
+		URI.parse(ngrok_config["tunnels"].first["public_url"]).host
+	end
+end
+
+Tilt.register Tilt::ERBTemplate, 'html.erb'
+
+configure do
+	set :domain, EnvironmentManager.get_domain_for_dev_env(ENV['RACK_ENV'])
+end
+
+get '/' do
+	erb :login
+end
+
+get '/s/:id' do
+	api = TinyShowAPI.new
+	@event = api.show(params[:id])
+	erb :show
+end
+
+get '/local_data' do
+	File.read('./public/data/BALTIMORE-09-21-2016-READY.json')
+end
