f87e9421c47cd1ea8b5c628a5dd3857a24cefd26
Tony Amoyal <tonyamoyal@gmail.com>
2016-11-21 16:39:57 -0400

    testing clock proc

--- config/clock.rb
+++ config/clock.rb
@@ -6,12 +6,10 @@ Bundler.require(:default)
 require "app/workers/aggregate_worker"
 
 module Clockwork
-  handler do |job, time|
+  #, :at => '06:00', :tz => 'EST'
+  every(5.minutes, 'get_facebook_events') do
     TinyShow.log_mode = :clock
-    TinyShow.info "Running #{job}, at #{time}"
-  end
-
-  every(1.day, 'get_facebook_events', :at => '06:00', :tz => 'EST') do
+    TinyShow.info "Running get_facebook_events, at #{Time.now}"
     w = AggregateWorker.new
     w.perform
     TinyShow.info "Finished get_facebook_events at #{Time.now}"
